1e1ddb2063667db046d7a204a826b59da3d628eb
Joseph Ahrens <Joseph Ahrens@Joseph-PC.resnet.central.edu>
2014-10-12 13:02:53 -0500

    Started Project

---
+++ src/improgram/IMProgram.java
@@ -0,0 +1,20 @@
+package improgram;
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Joseph Ahrens
+ */
+public class IMProgram {
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                MainGui gui = new MainGui();
+                gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                gui.setVisible(true);
+            }
+        })
+    }
+}
