e7858a6ab282e9f2020bedcc9f694fda0d17a0dc
Joseph Ahrens <Joseph Ahrens@Joseph-PC.resnet.central.edu>
2014-10-13 01:40:33 -0500

    Created instances of each object and completed the while loop that checks for what gui should be currently displayed

--- src/improgram/MainGui.java
+++ src/improgram/MainGui.java
@@ -8,10 +8,19 @@ import javax.swing.*;
 public class MainGui extends JFrame{
     public MainGui(){
         LoginDialog logInDialog = new LoginDialog(this, true);
-        logInDialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        logInDialog.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         
         RegisterDialog registerDialog = new RegisterDialog(this, true);
-        registerDialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        registerDialog.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        
+        MainWindowDialog mainDialog = new MainWindowDialog(this, true);
+        mainDialog.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        
+        EditBuddyListDialog editBuddyListDialog = new EditBuddyListDialog(this, true);
+        editBuddyListDialog.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        
+        MessagesDialog messagesDialog = new MessagesDialog(this, true);
+        messagesDialog.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         
         logInDialog.setVisible(true);
         
@@ -20,10 +29,36 @@ public class MainGui extends JFrame{
                 registerDialog.setVisible(true);
                 logInDialog.registerButtonClicked = false;
             }
-            if(registerDialog.registerButtonClicked){
+            if(logInDialog.logInButtonClicked){
+                mainDialog.setVisible(true);
+                logInDialog.logInButtonClicked = false;
+            }
+            if(registerDialog.registerButtonClicked || registerDialog.cancelButtonClicked){
                 logInDialog.setVisible(true);
                 registerDialog.registerButtonClicked = false;
+                registerDialog.cancelButtonClicked = false;
             }
+            if(mainDialog.editBuddyListMenuItemClicked){
+                editBuddyListDialog.setVisible(true);
+                mainDialog.editBuddyListMenuItemClicked = false;
+            }
+            if(mainDialog.messagesMenuItemClicked){
+                messagesDialog.setVisible(true);
+                mainDialog.messagesMenuItemClicked = false;
+            }
+            if(mainDialog.logOutButtonClicked){
+                logInDialog.setVisible(true);
+                mainDialog.logOutButtonClicked = false;
+            }
+            if(editBuddyListDialog.backButtonClicked){
+                mainDialog.setVisible(true);
+                editBuddyListDialog.backButtonClicked = false;
+            }
+            if(messagesDialog.backButtonClicked){
+                mainDialog.setVisible(true);
+                messagesDialog.backButtonClicked = false;
+            }
+            
         }
     }
     
